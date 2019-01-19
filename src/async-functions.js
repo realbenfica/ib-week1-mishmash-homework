2d5fb511251aa430c91e1cbea95313dc5e2d9b3d
“ALEX <alexgtr84@gmail.com>
2019-01-19 16:10:02 +0100

    ShoppingCart 4/4, Transform 3/5, async-functions 3/4

--- src/async-functions.js
+++ src/async-functions.js
@@ -1,14 +1,22 @@
-function giveItBackLater() {
-
+function giveItBackLater(value, callback) {
+    function loadComplete() {
+        callback(value)
+    }
+    setTimeout(loadComplete, 200)
 }
 
-function addSomePromises() {
-
+function promiseToGiveItBackLater(value) {
+    return new Promise((resolve) => {        
+        return setTimeout(() => resolve(value), giveItBackLater)      
+    })
+ 
 }
 
-function promiseToGiveItBackLater() {
-
+function addSomePromises(somePromise) {
+    somePromise
+        .then(function (value) {
+            return value;
+    })
 }
-
 
 module.exports = { giveItBackLater, addSomePromises, promiseToGiveItBackLater }
